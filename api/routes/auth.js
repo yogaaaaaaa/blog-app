a9785225e684cbc053116e4d84f64890640305a5
adhipratama <yogaadhi@icloud.com>
2021-12-23 13:40:54 +0700

    add middleware if user is exist

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -8,6 +8,12 @@ router.post("/register", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(req.body.password, salt);
 
+    const existUser = await User.findOne({email: req.body.email});
+
+    if(existUser){
+        return res.status(400).send("user is exist, please login");
+    }
+
     const newUser = new User({
       username: req.body.username,
       email: req.body.email,
@@ -21,5 +27,18 @@ router.post("/register", async (req, res) => {
     console.log(err);
   }
 });
+
+//login
+router.post("/login", async(req, res)=>{
+    try{
+        const user = await User.findOne({username: req.body.username});
+        !user && res.status(400).json("Wrong credentials!");
+
+        const validated = await bcrypt.compare(req.body.password, user.password)
+        !validated && res.status(400).json("Wrong credentials");
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
