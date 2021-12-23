f1c087b4b6e614db612b8f0bc5c4abfdcf885e0b
adhipratama <yogaadhi@icloud.com>
2021-12-23 10:50:47 +0700

    add user model

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
+const mongoose = require("mongoose");
 
 app.listen("5000", () => {
   console.log("backend is running at 5000");
 });
+mongoose.connect(process.env.MONGO_URL).then(console.log("db connected"));
 app.use("/", (req, res) => {
   console.log("hey this is main url");
 });
