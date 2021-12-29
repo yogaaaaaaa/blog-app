a1e7b6348e72d630a057083bf8c84366307ce03a
adhipratama <yogaadhi@icloud.com>
2021-12-29 11:28:41 +0700

    add delete user and post belong to user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const User = require("../models/User.js");
+const Post = require("../models/Post.js");
 const bcrypt = require("bcrypt");
 
-//register
+//update user
 router.put("/:id", async (req, res) => {
   if (req.body.userId === req.params.id) {
     if (req.body.password) {
@@ -10,15 +11,41 @@ router.put("/:id", async (req, res) => {
       req.body.password = await bcrypt.hash(req.body.password, salt);
     }
     try {
-      const updateUser = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        {
+          new: true,
+        }
+      );
       res.status(200).json(updateUser);
     } catch (err) {
       res.status(500).json(err);
     }
   } else {
     res.status(400).json("you can only update your account");
+  }
+});
+
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId == req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("user succesfully deleted...!");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("user not found");
+    }
+  } else {
+    res.status(401).json("you only able to delete your own account!");
   }
 });
 
