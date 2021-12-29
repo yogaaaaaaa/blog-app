95217740b5169facc1240cf0310d4c1c8c373ead
adhipratama <yogaadhi@icloud.com>
2021-12-29 12:43:48 +0700

    add get user by id

--- api/routes/users.js
+++ api/routes/users.js
@@ -64,4 +64,22 @@ router.delete("/:id", async (req, res) => {
   }
 });
 
+//get user
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id);
+  // console.log(user)
+
+  if (!user) {
+    res.status(404).json("there is no user with given id");
+  } else {
+    try {
+      const { password, ...others } = user._doc;
+      res.status(200).json(others);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
 module.exports = router;
