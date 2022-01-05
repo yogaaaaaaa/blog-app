c43835d958322261e15e987e4fc8b46931fae1d9
adhipratama <yogaadhi@icloud.com>
2022-01-05 11:40:58 +0700

    add image upload

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ const userRoute = require("./routes/users");
 const catRoute = require("./routes/categories");
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
+const multer = require("multer");
 
 app.use(express.json());
 app.listen(port, () => {
@@ -17,6 +18,23 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("db connected"))
   .catch((err) => console.log(err));
+
+//make storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "hello.jpg");
+  },
+});
+
+//route for upload
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("file succesfully uploaded!");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
