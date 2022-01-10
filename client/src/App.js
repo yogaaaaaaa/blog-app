2620536d930dcc26ae2b68e189b049124614d509
adhipratama <yogaadhi@icloud.com>
2022-01-10 12:51:32 +0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import Topbar from './topbar/Topbar.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+    </div>
+  );
+}
+
+export default App;
