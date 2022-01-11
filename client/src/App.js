b38d3246ae550578a844dc466e477f083224e31e
adhipratama <yogaadhi@icloud.com>
2022-01-11 11:42:24 +0700

    homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import Topbar from './topbar/Topbar.jsx';
+import Home from './pages/home/Home.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Topbar/>
-    </div>
+      <Home/>
+    </>
   );
 }
 
---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from './home.css'
+import Header from '../../header/Header.jsx'
+export default function Home() {
+    return (
+        <div className='home'>
+            <Header/>
+            homepage
+        </div>
+    )
+}
