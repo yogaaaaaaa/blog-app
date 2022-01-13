330ded55c99cc1fd9f247b4fb579f5f9f75f5030
adhipratama <yogaadhi@icloud.com>
2022-01-13 13:00:07 +0700

    adding single post

---
+++ client/src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,29 @@
+import "./singlePost.css";
+
+export default function SinglePost() {
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        <img
+          src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+          alt=""
+          className="singlePostImg"
+        />
+        <h1 className="singlePostTitle">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          <div className="singlePostEdit">
+            <i className="singlePostIcon far fa-edit"></i>
+            <i className="singlePostIcon far fa-trash-alt"></i>
+          </div>
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author: <b>Yoga</b>
+          </span>
+          <span className="singlePostDate">1 hour ago</span>
+        </div>
+        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+      </div>
+    </div>
+  );
+}
--- client/src/pages/single/Single.jsx
+++ client/src/pages/single/Single.jsx
@@ -1,10 +1,11 @@
 import './single.css'
 import Sidebar from '../../components/sidebar/Sidebar.jsx' 
+import SinglePost from '../../components/singlePost/SinglePost.jsx'
 
 export default function Single() {
     return (
         <div className='single'>
-            {/* post */}
+            <SinglePost />
             <Sidebar/>
         </div>
     )
