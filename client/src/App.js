f758441bd8ddf9a6f59ae6eb8ec0a4230ffc3bfa
adhipratama <yogaadhi@icloud.com>
2022-01-24 14:38:07 +0700

    adding react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 // import logo from './logo.svg';
-import Topbar from './components/topbar/Topbar.jsx';
-import Single from './pages/single/Single.jsx';
-import Write from './pages/write/Write.jsx';
-import Settings from './pages/settings/Settings.jsx'
-import Login from './pages/login/Login.jsx'
-import Register from './pages/register/Register.jsx'
+import Topbar from "./components/topbar/Topbar.jsx";
+import Home from "./pages/home/Home.jsx";
+import Single from "./pages/single/Single.jsx";
+import Write from "./pages/write/Write.jsx";
+import Settings from "./pages/settings/Settings.jsx";
+import Login from "./pages/login/Login.jsx";
+import Register from "./pages/register/Register.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Topbar/>
-      {/* <Settings/> */}
-      <Register />
-    </>
+    <Router>
+      <Topbar />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
