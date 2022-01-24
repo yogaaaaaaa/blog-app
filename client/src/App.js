702613c035452ad92c941de72315b7bd338f0ca5
adhipratama <yogaadhi@icloud.com>
2022-01-24 13:02:51 +0700

    add registration pages

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,14 @@ import Single from './pages/single/Single.jsx';
 import Write from './pages/write/Write.jsx';
 import Settings from './pages/settings/Settings.jsx'
 import Login from './pages/login/Login.jsx'
+import Register from './pages/register/Register.jsx'
 
 function App() {
   return (
     <>
       <Topbar/>
       {/* <Settings/> */}
-      <Login />
+      <Register />
     </>
   );
 }
