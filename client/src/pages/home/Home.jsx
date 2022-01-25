fcb40059dbbe4313fa289dfd4633978314a5eeb7
adhipratama <yogaadhi@icloud.com>
2022-01-25 16:32:26 +0700

    adding axios

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from "./home.css";
 import Header from "../../components/header/Header.jsx";
 import Posts from "../../components/posts/Posts.jsx";
 import Sidebar from "../../components/sidebar/Sidebar.jsx";
+import "./home.css";
 
 export default function Home() {
   return (
