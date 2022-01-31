06fa80cb18976d0281bc3e8e1879bf434b915425
adhipratama <yogaadhi@icloud.com>
2022-01-31 14:39:06 +0700

    context, reduceer, actions ongoing'

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,7 +1,7 @@
-import Reducer from "./Reducer.js";
 import { createContext, useReducer } from "react";
+import Reducer from "./Reducer";
 
-const INITITAL_STATE = {
+const INITIAL_STATE = {
   user: null,
   isFetching: false,
   error: false,
@@ -12,12 +12,17 @@ export const Context = createContext(INITIAL_STATE);
 export const ContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(Reducer, INITIAL_STATE);
 
-  return(
-      <Context.Provider value={{
-       user:state.user,
-       isFetching:state.isFetching,
-       error:state.error}} >
-           {children}  
-      </Context.Provider>
-  )
+
+  return (
+    <Context.Provider
+      value={{
+        user: state.user,
+        isFetching: state.isFetching,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 };
--- client/src/context/Reducer.js
+++ client/src/context/Reducer.js
@@ -1,23 +1,23 @@
 const Reducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN_START":
-      return {
-        user: null,
-        isFetching: true,
-        error: false,
-      };
+    switch (action.type) {
+      case "LOGIN_START":
+        return {
+          user: null,
+          isFetching: true,
+          error: false,
+        };
       case "LOGIN_SUCCESS":
-      return {
-        user: action.payload,
-        isFetching: false,
-        error: false,
-      };
+        return {
+          user: action.payload,
+          isFetching: false,
+          error: false,
+        };
       case "LOGIN_FAILURE":
-      return {
-        user: null,
-        isFetching: false,
-        error: true,
-      };
+        return {
+          user: null,
+          isFetching: false,
+          error: true,
+        };
       default:
           return state;
   }
